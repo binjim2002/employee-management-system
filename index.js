d441b5bce933fdd5c7f2b475a5fd33786eb17811
binjim2002 <binjim2002@yahoo.fr>
2020-07-09 23:42:31 -0400

    working on the prompts

--- index.js
+++ index.js
@@ -12,3 +12,27 @@ const mainMenuQuestions = [
         choices: ['Add department', 'Add employee', 'Add role', 'View employees', 'View departments', 'View roles']
     }
 ];
+class Program {
+    init(){
+        
+        this.employee = [];
+        this.role = [];
+        this.department = [];
+        ['employee','role','department'].forEach(table => {
+            this.getTable(table,res=>{});
+        })
+        
+    }
+    start(){
+        this.prompt(mainMenuQuestions, result => {
+            const parts = result.action.split(' ');
+            switch(parts[0]){
+                case 'Add':
+                    this.add(parts[1]);
+                    break;
+                case 'View':
+                    this.view(parts[1].substring(0,parts[1].length-1));
+                    break;
+            }
+        })
+    }
